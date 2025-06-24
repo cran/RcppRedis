512fbc5ba15318a48ec80d3f21fb597aa81e2c3c
Dirk Eddelbuettel <edd@debian.org>
2025-06-24 14:30:02 +0000

    version 0.2.6

--- inst/tinytest/test_list.R
+++ inst/tinytest/test_list.R
@@ -5,7 +5,7 @@ redis <- new(Redis)
 exampleNumericElement <- 1.0
 exampleCharElement <- "a"
 key <- "RcppRedis:test:mylist"
-redis$exec(paste("del", key))
+expect_equal(redis$del(key), 0)
 
 
 #test_02_lpush <- function() {
@@ -60,10 +60,42 @@ redis$lpush(key, 3)
 redis$ltrim(key, 0, 1)
 res <- redis$lrange(key, 0, 100)
 expect_equal(res,list(3,2))
-redis$exec(paste("del", key))
+expect_equal(redis$del(key), 1)
 
 
 #test_09_cleanup <- function() {
 ## delete key
-n <- redis$exec(paste("del", key))
-expect_equal(n, 0)
+expect_equal(redis$del(key), 0)
+
+
+## check lrem
+redis$del(key)
+elem <- "abc"
+redis$lpush(key, elem)
+redis$lpush(key, elem)
+redis$lpush(key, elem)
+redis$lpush(key, elem)
+expect_equal(redis$llen(key), 4)
+redis$lrem(key, 1, elem)
+expect_equal(redis$llen(key), 3)
+redis$lrem(key, -1, elem)
+expect_equal(redis$llen(key), 2)
+expect_equal(redis$lpop(key), elem)
+expect_equal(redis$lpop(key), elem)
+expect_equal(redis$keys(key), character())
+
+## check lmove
+altkey <- "RcppRedis:test:myotherlist"
+redis$del(altkey)
+redis$rpush(key, 1)
+redis$rpush(key, 2)
+redis$rpush(key, 3)
+redis$lmove(key, altkey, "LEFT", "RIGHT")
+expect_equal(redis$llen(key), 2)
+expect_equal(redis$llen(altkey), 1)
+redis$lmove(key, altkey, "LEFT", "LEFT")
+expect_equal(redis$llen(key), 1)
+expect_equal(redis$llen(altkey), 2)
+expect_equal(redis$lpop(key), 3)    # as 1 and 2 have been moved (both from left)
+expect_equal(redis$lpop(altkey), 2) # as 2 was inserted to the left
+expect_equal(redis$lpop(altkey), 1) # as 1 remains
